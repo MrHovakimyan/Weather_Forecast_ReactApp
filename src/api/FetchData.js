428ca2d486c4a038e5b75efac738e2a1094fb32c
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-07-28 21:37:07 +0400

    feat: Home page w/getting-rendering data created

---
+++ src/api/FetchData.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BASE_URL } from "./configs";
+
+export async function FetchData(latitude, longitude) {
+  try {
+    return fetch(
+      `${BASE_URL}?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max&timezone=UTC`
+    )
+      .then((response) => response.json())
+      .then((results) => {
+        return results.daily.temperature_2m_max;
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { FetchData } from "../api/FetchData";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [forecast, setForecast] = useState([]);
+
+  const handleLatChange = (evn) => {
+    let latitudeForSearch = parseFloat(evn.target.value);
+    setLatitude(latitudeForSearch);
+  };
+  const handleLongChange = (evn) => {
+    let longitudeForSearch = parseFloat(evn.target.value);
+    setLongitude(longitudeForSearch);
+  };
+
+  const loadedData = async (latitude, longitude) => {
+    let results = await FetchData(latitude, longitude);
+    return setForecast(results);
+  };
+
+  const handleReset = () => {
+    setLatitude("");
+    setLongitude("");
+    setForecast([]);
+  };
+
+  return (
+    <div className="home-content">
+      <div className="home-content-search">
+        <input
+          type="number"
+          placeholder="Enter latitude"
+          value={latitude}
+          onChange={handleLatChange}
+        />
+        <input
+          type="number"
+          placeholder="Enter longitude"
+          value={longitude}
+          onChange={handleLongChange}
+        />
+      </div>
+
+      <div className="home-content-buttonsWrpr">
+        <button
+          className="home-content-buttonsWrpr-btn"
+          onClick={() => loadedData(latitude, longitude)}
+        >
+          Search
+        </button>
+        <button className="home-content-buttonsWrpr-btn" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+      {forecast &&
+        forecast.map((item, index) => (
+          <div className="home-content-forecast" key={index}>
+            {item}
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Home;
